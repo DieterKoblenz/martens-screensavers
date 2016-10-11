cd14dac730cab3237f8c02a7e652d575cd78deae
DieterKoblenz <mail@jeroenvanveen.nl>
2016-10-11 10:16:16 +0200

    Added slow rainbow

--- app.js
+++ app.js
@@ -333,3 +333,113 @@ animation_red_slow_sleep.register(function(err, result){
 	})
 })
 //-------------------red Slow Sleep Animation Stop ---------------------------------------------------
+//-------------------Slow Rainbow Start ---------------------------------------------------
+var frames_slow_rainbow = [];
+var frame_slow_rainbow = [];
+
+// for every pixel...
+for( var pixel = 0; pixel < 24; pixel++ ) {
+
+    var hue = (pixel/(24)) * 360;
+    var color = hsvToRgbLow( hue, 100, 100 )
+
+    frame_faint_rainbow.push({
+        r: color[0], // 0 - 255
+        g: color[1], // 0 - 255
+        b: color[2]  // 0 - 255
+    });
+}
+
+function hsvToRgbLow(h, s, v) {
+    var r, g, b;
+    var i;
+    var f, p, q, t;
+
+    // Make sure our arguments stay in-range
+    h = Math.max(0, Math.min(360, h));
+    s = Math.max(0, Math.min(100, s));
+    v = Math.max(0, Math.min(100, v));
+
+    // We accept saturation and value arguments from 0 to 100 because that's
+    // how Photoshop represents those values. Internally, however, the
+    // saturation and value are calculated from a range of 0 to 1. We make
+    // That conversion here.
+    s /= 100;
+    v /= 100;
+
+    if(s == 0) {
+        // Achromatic (grey)
+        r = g = b = v;
+        return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+    }
+
+    h /= 60; // sector 0 to 5
+    i = Math.floor(h);
+    f = h - i; // factorial part of h
+    p = v * (1 - s);
+    q = v * (1 - s * f);
+    t = v * (1 - s * (1 - f));
+
+    switch(i) {
+        case 0:
+            r = v;
+            g = t;
+            b = p;
+            break;
+
+        case 1:
+            r = q;
+            g = v;
+            b = p;
+            break;
+
+        case 2:
+            r = p;
+            g = v;
+            b = t;
+            break;
+
+        case 3:
+            r = p;
+            g = q;
+            b = v;
+            break;
+
+        case 4:
+            r = t;
+            g = p;
+            b = v;
+            break;
+
+        default: // case 5:
+            r = v;
+            g = p;
+            b = q;
+    }
+
+    return [Math.round(r * 25), Math.round(g * 25), Math.round(b * 25)];
+}
+
+frames_slow_rainbow.push(frame_slow_rainbow);
+
+var animation_slow_rainbow = new Animation({
+	
+    options: {
+        fps     : 1, 	// real frames per second
+        tfps    : 60, 	// target frames per second. this means that every frame will be interpolated 60 times
+        rpm     : 1,	// rotations per minute
+    },
+    frames    : frames_slow_rainbow
+})
+
+animation_slow_rainbow.register(function(err, result){
+	Homey.manager('ledring').registerScreensaver('faint_rainbow', animation_slow_rainbow)
+	if( err ) return Homey.error(err);
+	animation_slow_rainbow.on('screensaver_start', function( screensaver_id ){
+		Homey.log('Screensaver started')
+	})
+	animation_slow_rainbow.on('screensaver_stop', function( screensaver_id ){
+		Homey.log('Screensaver stopped')
+	})
+})
+//-------------------Slow Rainbow Animation Stop ---------------------------------------------------
